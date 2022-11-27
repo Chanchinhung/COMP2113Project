72589bfd4bb786c0f483d596890ab1df95452a35
Chan Chin Hung <h3578862@academy11.cs.hku.hk>
2022-11-27 23:34:49 +0800

    LoadWLstat.cpp committed

---
+++ loadWLstat.cpp
@@ -0,0 +1,20 @@
+#include<fstream>
+#include<Cards.h>
+using namespace std;
+int loadWLstat(vector<RoundStat> WLrec){
+  char filename[] = "WLstat.txt";
+  char WL;
+  int bet;
+  RoundStat temp;
+  ifstream fin;
+  fin.open(filename);
+  if (fin.fail()){
+    cout << "Error in file opening, please contact the responsible personnel" << endl;
+    exit(1);
+  }
+  while (fin >> temp.WL){
+    fin >> temp.moneywon;
+    WLrec.push_back(temp);
+  }
+  return 0;
+}
