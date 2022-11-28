bd455c91d69150a715473e4f3a5d4a0458f3e536
samkam108 <114809377+samkam108@users.noreply.github.com>
2022-11-28 23:52:39 +0800

    Update Cards.h

--- Cards.h
+++ Cards.h
@@ -1,8 +1,8 @@
 #ifndef Cards_H //header file for data structure storing cards hit by house and player
 #define Cards_H
 struct Cards{
-  int cardval;
-  char cardface;
-  Cards *next;
+  int cardval; //The numeric value of the card
+  char cardface; //The face of the card
+  Cards *next; //The next node
 };
 #endif
