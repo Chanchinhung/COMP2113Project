25f6c68e8c9a83e9cbef3ffc4b10407a167ebe16
samkam108 <114809377+samkam108@users.noreply.github.com>
2022-12-01 00:15:49 +0800

    Update DealerProcess.cpp

--- DealerProcess.cpp
+++ DealerProcess.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+//Incomplete
+//This program is to process the dealer's turn fully using the functions created previously. 
+//This program will compare the dealers' card result and the player's card result, and determine who wins the game and print out the appropriate text content.
 using namespace std;
 
 
