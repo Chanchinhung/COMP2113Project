61e5f53a5ac63c74ea41a86bbf2d952ae72bc239
Ryan Reese <thenewteadrinker@gmail.com>
2022-12-01 17:00:52 +0800

    Changed to "Card.h"

--- Cards.h
+++
@@ -1,8 +0,0 @@
-#ifndef Cards_H //header file for data structure storing cards hit by house and player
-#define Cards_H
-struct Cards{
-  int cardval; //The numeric value of the card
-  char cardface; //The face of the card
-  Cards *next; //The next node
-};
-#endif
