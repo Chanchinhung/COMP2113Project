8f5c5afd42de5de10f0b99176983625f8027e668
samkam108 <114809377+samkam108@users.noreply.github.com>
2022-12-02 18:31:08 +0800

    Update SplitCards.h

--- SplitCards.h
+++ SplitCards.h
@@ -1,12 +1,15 @@
 //This is a headerfile of SplitCards.cpp
-#include "Card.h"
+#include <iostream>
+#include <iomanip>
 #include <vector>
+#include "Card.h"
+#include "RoundStat.h"
 
 #ifndef SPLITCARDS_H
 #define SPLITCARDS_H
 
 using namespace std;
 bool samecard( vector<card> & )
-void split( vector<card> & );
+void split(int &, int &, int &, vector<RoundStat> &, vector<card> &, vector<card> &);
 
 #endif
