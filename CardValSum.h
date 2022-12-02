a0f11b24340a75e9808c34440864d8ceeed93f11
Ryan Reese <thenewteadrinker@gmail.com>
2022-12-02 23:31:44 +0800

    Deleted .o files from main branch and updated DetermineWinner.h

--- CardValSum.h
+++
@@ -1,6 +0,0 @@
-//CardValSum.h, for function that returns total value of a hand
-#ifndef CardValSum_H
-#define CardValSum_H
-#include "Cards.h"
-int CardValSum(Cards *head);
-#endif
--- DetermineWinner.h
+++ DetermineWinner.h
@@ -1,8 +1,6 @@
 #include <vector>
 #include "Card.h"
-#include "CardValSum.h"
 #ifndef DetermineWinner_H
 #define DetermineWinner_H
 int DetermineWinner(vector<card> &, vector <card> &);
-int NoAces(int, int);
 #endif
