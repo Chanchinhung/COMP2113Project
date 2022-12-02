e37c293ce4d8133e883ddb93649c52bd3db27f3e
samkam108 <114809377+samkam108@users.noreply.github.com>
2022-12-03 00:30:11 +0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -6,9 +6,13 @@ class card {
         void display();
         bool is_ace();
         int get_value();
-    private:
+    //private:
         int value;
         char number;
         char suit;
+    //For the Playround function, I runned the code in a compiler, and it seems that it is not working well
+    //because of the "private:".
+    //I did not learn "class" so I don't know why it is happening. However, after deleting the "private:",
+    //All things are working well. So let's discuss this in our meeting later on, whether to delete it or modify the playround function.
 };
 #endif
