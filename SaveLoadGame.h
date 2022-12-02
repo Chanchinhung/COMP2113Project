acec4a9a387efd13bee047e065a5161d3ac86d4d
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-03 01:36:12 +0800

    Create SaveLoadGame.h

---
+++ SaveLoadGame.h
@@ -0,0 +1,7 @@
+#ifndef SaveLoadGame_H
+#define SaveLoadGame_H
+
+void LoadGame (int &playermoney, int &housemoney, vector<RoundStat> &WLrec);
+void SaveGame (int playermoney, int housemoney, vector<RoundStat> WLrec);
+
+#endif
