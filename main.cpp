8d77f1940e171b83ad8133ed14efe5bdce4593ed
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-03 02:10:12 +0800

    add parameter to function

--- main.cpp
+++ main.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <vector>
 #include "GameMenu.h"
+#include "RoundStat.h"
 using namespace std;
 
 int main(){
-    // global variable declaration
-    int player_money = 1000, house_money = 1000;
-    int arrsize=0;		// for "winstat" array sizing
-    char *winstat;
-    winstat = new char[arrsize];
-    bool Won;
+	// global variable declaration
+	int playermoney = 1000, housemoney = 1000;
+	vector<RoundStat> WLrec;
+	
+	// display start screen
+	display_start();
 
-    // display start screen
-    display_start();
 	// display start menu
-	game_menu_1();
+	game_menu_1(playermoney, housemoney, WLrec);
 	// display pause menu
-	game_menu_2();
-
-    // sizing the win-loss array
-	char *temp; temp = new char[arrsize];
-	for (int i=0; i<arrsize; i++) {
-		temp[i] = winstat[i];
-	}
-	delete [] winstat;
-	arrsize += 1;
-	winstat = new char[arrsize];
-	for (int i=0; i<arrsize-1; i++)
-		winstat[i] = temp[i];
-	delete [] temp;
-	// append the win-loss history to the new array
-	if (Won)
-		winstat[arrsize-1] = 'W';
-	else
-		winstat[arrsize-1] = 'L';
+	game_menu_2(playermoney, housemoney, WLrec);
 	
 	//display exit menu
 	display_exit();
-    return 0;
+	return 0;
 }
