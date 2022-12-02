34e0ebd6a1d1a4f347fb0f27ef728f035fbb7b0d
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-03 01:45:49 +0800

    included save/load game function

--- GameMenu.cpp
+++ GameMenu.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "GameMenu.h"
+#include "SaveLoadGame.h"
 using namespace std;
 
 //start screen
 void display_start() {
 	cout << "===============================" << endl;
-    cout << "Welcome to the Blackjack Casino" << endl;
-    cout << "===============================" << endl;
+	cout << "Welcome to the Blackjack Casino" << endl;
+	cout << "===============================" << endl;
 }
 
 void display_gm1() {
@@ -20,7 +21,7 @@ void display_gm1() {
 //start menu
 void game_menu_1() { 
 	char choice;
-    display_gm1();
+	display_gm1();
 	cin >> choice;
 	while (!((choice=='n') || (choice=='l') || (choice=='q'))) {
 		cout << "Invalid input, try again." << endl;
@@ -33,9 +34,10 @@ void game_menu_1() {
 	}
 	else if (choice=='l') {
 		//implement load game
+		LoadGame(playermoney,housemoney,WLrec);
 		exit(1);
 	}
-    else if (choice=='q') {
+	else if (choice=='q') {
         //implement quit game
 		display_exit();
 		exit(1);
@@ -43,7 +45,7 @@ void game_menu_1() {
 }
 
 void display_gm2() {
-    cout << "Options:" << endl;
+	cout << "Options:" << endl;
 	cout << "Continue (c)" << endl;
 	cout << "New Game (n)" << endl;
 	cout << "Save game (s)" << endl;
@@ -72,6 +74,7 @@ void game_menu_2() {
 	}
 	else if (choice=='s') {
 		//implement save game
+		SaveGame(playermoney,housemoney,WLrec);
 		exit(1);
 	}
 	else if (choice=='q') {
@@ -84,7 +87,7 @@ void game_menu_2() {
 //exit screen
 void display_exit() {
 	cout << "===================================================" << endl;
-    cout << "Thank you for playing Blackjack. See you next time!" << endl;
-    cout << "===================================================" << endl;
+	cout << "Thank you for playing Blackjack. See you next time!" << endl;
+	cout << "===================================================" << endl;
 	cout << "By Sam, Ryan, Arnold, and Zay." << endl;
 }
