a2aec8096f678993cd083b035476fe362bbf4db2
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-03 12:43:57 +0800

    Added - using namespace std;

--- DetermineWinner.h
+++ DetermineWinner.h
@@ -1,6 +1,11 @@
 #include <vector>
 #include "Card.h"
+
 #ifndef DetermineWinner_H
 #define DetermineWinner_H
+
+using namespace std;
+
 int DetermineWinner(vector<card> &, vector <card> &);
+
 #endif
