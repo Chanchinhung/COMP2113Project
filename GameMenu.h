27dec444751a631f26057023e0b59a04b4ae8549
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-03 12:15:41 +0800

    add header file and parameter in functions

--- GameMenu.h
+++ GameMenu.h
@@ -1,11 +1,16 @@
+#include <vector>
+#include "RoundStat.h"
+
 #ifndef GAMEMENU_H
 #define GAMEMENU_H
 
+using namespace std;
+
 void display_start();
 void display_gm1();
-void game_menu_1();
+void game_menu_1(int &, int &, vector<RoundStat> &);
 void display_gm2();
-void game_menu_2();
+void game_menu_2(int &, int &, vector<RoundStat> &);
 void display_exit();
 
 #endif
