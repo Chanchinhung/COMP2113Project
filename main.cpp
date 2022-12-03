2f3225bddafcb5f4f5cfe180af42fded1d4913b4
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-04 03:05:21 +0800

    indentation+comment

--- main.cpp
+++ main.cpp
@@ -7,19 +7,14 @@
 using namespace std;
 
 int main(){
-	// global variable declaration
-	int playermoney = 1000, housemoney = 1000;
-	vector<RoundStat> WLrec;
-	// display start screen
-	display_start();
-	// display start menu
-	game_menu_1(playermoney, housemoney, WLrec);
-	// Note that the game loop is in GameMenu.cpp
-	
-	// display pause menu
-	//game_menu_2(playermoney, housemoney, WLrec);
-	
-	//display exit menu
-	//display_exit();
-	return 0;
+    // global variable declaration
+    int playermoney = 1000, housemoney = 1000;
+    vector<RoundStat> WLrec;
+    // display start screen
+    display_start();
+    // display start menu
+    game_menu_1(playermoney, housemoney, WLrec);
+    // Note that the game loop is in GameMenu.cpp
+
+    return 0;
 }
