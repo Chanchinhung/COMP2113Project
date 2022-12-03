9ac31402b7cc71d7d808910a70c79ac0b0f33980
aaaaaaaaaalung71 <116965261+aaaaaaaaaalung71@users.noreply.github.com>
2022-12-04 03:11:35 +0800

    indentation+comment

--- SaveLoadGame.cpp
+++ SaveLoadGame.cpp
@@ -8,77 +8,60 @@ using namespace std;
 
 // Function to Load the Game (Load the player/house money; Load the WL stat)
 void LoadGame (int &playermoney, int &housemoney, vector<RoundStat> &WLrec){
-	// read the file
-	char filename[] = "game_save.txt";
-	ifstream fin;
-	fin.open(filename);
-	if (fin.fail()){
-		cout << "Error in loading the game status." << endl;
-		exit(1);
-	}
-
-	// read the player and house money balance from the file
-	fin >> playermoney;
-	fin >> housemoney;
-
-	// load the W-L record from the file
-	RoundStat temp;
-	while (fin >> temp.WL){
-		fin >> temp.moneywon;
-		WLrec.push_back(temp);
-	}
-
-	fin.close();
+    // read the file
+    char filename[] = "game_save.txt";
+    ifstream fin;
+    fin.open(filename);
 	
-	// Screen output money balance
-	cout << "Game loaded successfully." <<endl;
-	cout << "Bank Balance:" << endl;
-	cout << "Your money:    $" << playermoney << endl;
-	cout << "House money: $" << housemoney << endl;
+    // if fail opening the file then exit the function
+    if (fin.fail()){
+        cout << "Error in loading the game status." << endl;
+        return;
+    }
+
+    // read the player and house money balance from the file
+    fin >> playermoney;
+    fin >> housemoney;
+
+    // load the W-L record from the file
+    RoundStat temp;
+    while (fin >> temp.WL){
+        fin >> temp.moneywon;
+        WLrec.push_back(temp);
+    }
+
+    fin.close();
+	
+    // Screen output money balance
+    cout << "Game loaded successfully." <<endl;
+    cout << "Bank Balance:" << endl;
+    cout << "Your money:    $" << playermoney << endl;
+    cout << "House money: $" << housemoney << endl;
 }
 
 // Function to Save the Game (save the player/house money; save the WL stat)
 void SaveGame (int playermoney, int housemoney, vector<RoundStat> WLrec){
-	// Create the File
-	char filename[] = "game_save.txt";
-	ofstream fout;
-	fout.open(filename);
-	if (fout.fail()){
-		cout << "Error in saving the game status." << endl;
-		exit(1);
-	}
+    // Create the File
+    char filename[] = "game_save.txt";
+    ofstream fout;
+    fout.open(filename);
 
-	// Write the playermoney and Housemoney amount into the file
-	fout << playermoney << " " << housemoney << endl;
+    // if fail saving the file then exit the function
+    if (fout.fail()){
+        cout << "Error in saving the game status." << endl;
+        return;
+    }
 
-	// Save the W-L record to the file
-	for (int i = 0; i < WLrec.size(); i++){
-		fout << WLrec[i].WL << " " << WLrec[i].moneywon << endl;
-	}
+    // Write the playermoney and Housemoney amount into the file
+    fout << playermoney << " " << housemoney << endl;
 
-	fout.close();
-	
-	// Screen output
-	cout << "The game has successfully been saved!" << endl;
+    // Save the W-L record to the file
+    for (int i = 0; i < WLrec.size(); i++){
+        fout << WLrec[i].WL << " " << WLrec[i].moneywon << endl;
+    }
+
+    fout.close();
+
+    // Screen output
+    cout << "The game has successfully been saved!" << endl;
 }
-
-
-// main program for testing this module
-
-// int main(){ 
-// 	int playermoney;
-// 	int housemoney;
-// 	vector<RoundStat> WLrec;
-// 	// RoundStat temp;
-// 	// temp.WL = 'D';
-// 	// temp.moneywon = 300;
-// 	// WLrec.push_back(temp);
-// 	// WLrec.push_back(temp);
-// 	// SaveGame(playermoney,housemoney,WLrec);
-// 	LoadGame(playermoney,housemoney,WLrec);
-// 	cout << playermoney << " " << housemoney<< endl;
-// 	for (int i = 0; i < WLrec.size(); i++){
-// 		cout << WLrec[i].WL << " " << WLrec[i].moneywon << endl;
-// 	}
-// 	return 0;
-// }
