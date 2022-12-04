9177148edfa1ed50a67688f639634230ef010a48
Chan Chin Hung <h3578862@academy21.cs.hku.hk>
2022-12-04 16:50:14 +0800

    removed unneeded files, initialised save file

---
+++ SplitCards.h
@@ -0,0 +1,15 @@
+//This is a headerfile of SplitCards.cpp
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "Card.h"
+#include "RoundStat.h"
+
+#ifndef SPLITCARDS_H
+#define SPLITCARDS_H
+
+using namespace std;
+bool samecard( vector<card> & );
+void split(int &, int &, int &, bool &, bool &, vector<RoundStat> &, vector<card> &, vector<card> &);
+
+#endif
